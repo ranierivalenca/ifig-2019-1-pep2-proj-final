fe965e18eb9e1f2f15bed6de10ba5b4bd183cae0
kevinsousa <kevinsmoura@hotmail.com>
2019-07-13 18:52:56 -0300

    #11 Criação de migration de turmas

---
+++ pro_final/database/migrations/2019_07_13_195710_turma.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Turma extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('turma', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('semestres');
+            $table->string('turno');
+            $table->timestamps();
+            
+            $table->Integer('professor_id')->unsigned();
+            $table->foreign('professor_id')->references('id')->on('professor')->onDelete('cascade');
+            
+            $table->Integer('disciplinas_id')->unsigned();
+            $table->foreign('disciplinas_id')->references('id')->on('disciplinas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('turma');
+    }
+}
